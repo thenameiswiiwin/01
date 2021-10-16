283e2a7454a3f1883e2852aec930b7fa13e11934
Huy Nguyen <huyn.nguyen95@gmail.com>
2021-10-15 23:29:26 -0500

    finished Header for Layout component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,32 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: {
+      primary: 'rgb(236, 236, 236)',
+      'primary-2': 'rgb(220, 220, 220)',
+      secondary: 'rgb(29, 25, 26)',
+    },
     extend: {
+      maxHeight: {
+        '9/10': '90%',
+      },
+      inset: {
+        '2/25': '8%',
+      },
+      margin: {
+        '1/20': '5%',
+      },
+      padding: {
+        '1/100': '1%',
+        '7/100': '7%',
+      },
       borderWidth: {
         1: '1px',
       },
       textColor: {
-        secondary: '#d13f3f',
+        primary: 'rgb(29, 25, 26)',
+        secondary: 'rgb(236, 236, 236)',
+        orange: '#d13f3f',
       },
       fontSize: {
         2: '2vw',
